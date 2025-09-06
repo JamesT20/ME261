4a1df0128b0217840d4f7ffa06cfcba3ea70212b
James <jamestorok63@gmail.com>
2025-09-06 08:53:19 -0600

    fixed folder structure

--- echoname.m
+++
@@ -1,7 +0,0 @@
-% Problem 2: this script echos the name
-
-% get name input as raw input
-name = input('Enter your name: ','s');
-
-% fprint formatted message
-fprintf('Wow! Your name is %s!!\n', name)
