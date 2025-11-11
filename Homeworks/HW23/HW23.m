cb168bb98dcd8fd447bb09dd60436b59c1dc00c0
James Torok <jamest20@nmsu.edu>
2025-11-11 15:30:03 -0700

    HW23 initial commit

--- Homeworks/HW23/HW23.m
+++ Homeworks/HW23/HW23.m
@@ -1,4 +1,17 @@
 clc
 clear
-x = linspace(0,9,10);
-y = [9.45 4.35 3.65 4.20 4.55 6.10 6.90 9.25 11.40 14.26];
+close all
+x =  [0 1 2 3 4 5 6 7 8 9]';
+y = [9.45 4.35 3.65 4.20 4.55 6.10 6.90 9.25 11.40 14.26]';
+n = length(x);
+Z = [ones(n,1) x x.^2];
+
+A = Z'*Z;
+b = Z'*y;
+c = A\b; % same as a = inv(A)*b
+xx = linspace(0, 9, 50);
+yy = c(1) + c(2)./(xx + 1).^(2) + c(3)*xx.^2;
+for i=c
+    i
+end
+plot(x,y,'o',xx,yy)
